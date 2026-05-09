916c45c228310e03a030a90f1d1fd6753112f8f9
thilini22055 <thiliniyashoda10@gmail.com>
2026-05-09 13:10:05 +0530

    Add Sorting Visualizer UI layout with controls and panel

--- AlgorithmVisualizer/SortingForm.Designer.cs
+++ AlgorithmVisualizer/SortingForm.Designer.cs
@@ -28,19 +28,121 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnGenarate = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.lblComparisons = new System.Windows.Forms.Label();
+            this.cmbAlgorithm = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnStart.Font = new System.Drawing.Font("Segoe UI Symbol", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.ForeColor = System.Drawing.Color.Green;
+            this.btnStart.Location = new System.Drawing.Point(42, 291);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(121, 37);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "▶ Start";
+            this.btnStart.UseVisualStyleBackColor = false;
+            // 
+            // btnReset
+            // 
+            this.btnReset.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnReset.Font = new System.Drawing.Font("Segoe UI Symbol", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btnReset.Location = new System.Drawing.Point(231, 291);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(113, 38);
+            this.btnReset.TabIndex = 1;
+            this.btnReset.Text = "🔄 Reset";
+            this.btnReset.UseVisualStyleBackColor = false;
+            // 
+            // btnGenarate
+            // 
+            this.btnGenarate.BackColor = System.Drawing.Color.Cyan;
+            this.btnGenarate.Font = new System.Drawing.Font("Segoe UI Symbol", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGenarate.ForeColor = System.Drawing.Color.Navy;
+            this.btnGenarate.Location = new System.Drawing.Point(409, 291);
+            this.btnGenarate.Name = "btnGenarate";
+            this.btnGenarate.Size = new System.Drawing.Size(135, 38);
+            this.btnGenarate.TabIndex = 2;
+            this.btnGenarate.Text = "➕ Genarate";
+            this.btnGenarate.UseVisualStyleBackColor = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(776, 232);
+            this.panel1.TabIndex = 3;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Silver;
+            this.button4.Font = new System.Drawing.Font("Segoe UI Symbol", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(606, 290);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(122, 38);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "⚙ Settings";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // lblComparisons
+            // 
+            this.lblComparisons.AutoSize = true;
+            this.lblComparisons.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblComparisons.Location = new System.Drawing.Point(233, 359);
+            this.lblComparisons.Name = "lblComparisons";
+            this.lblComparisons.Size = new System.Drawing.Size(109, 18);
+            this.lblComparisons.TabIndex = 5;
+            this.lblComparisons.Text = "Comparisons";
+            // 
+            // cmbAlgorithm
+            // 
+            this.cmbAlgorithm.FormattingEnabled = true;
+            this.cmbAlgorithm.Items.AddRange(new object[] {
+            "Insertion Sort",
+            "Quick Sort"});
+            this.cmbAlgorithm.Location = new System.Drawing.Point(42, 359);
+            this.cmbAlgorithm.Name = "cmbAlgorithm";
+            this.cmbAlgorithm.Size = new System.Drawing.Size(121, 24);
+            this.cmbAlgorithm.TabIndex = 6;
             // 
             // SortingForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cmbAlgorithm);
+            this.Controls.Add(this.lblComparisons);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnGenarate);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnStart);
             this.Name = "SortingForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Sorting Form";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnGenarate;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label lblComparisons;
+        private System.Windows.Forms.ComboBox cmbAlgorithm;
     }
 }
