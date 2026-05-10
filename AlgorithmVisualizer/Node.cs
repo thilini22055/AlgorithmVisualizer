7d219283d48cf13f862caee646783355f278096b
thilini22055 <thiliniyashoda10@gmail.com>
2026-05-10 20:19:17 +0530

    Finish PathfindingForm core functionality: grid creation, BFS pathfinding, and visual rendering. Settings feature not implemented yet.

--- AlgorithmVisualizer/Node.cs
+++ AlgorithmVisualizer/Node.cs
@@ -6,28 +6,23 @@ using System.Threading.Tasks;
 
 namespace AlgorithmVisualizer
 {
-    
-        internal class Node
+    internal class Node
+    {
+        public int Row;
+        public int Col;
+
+        public bool IsWall = false;
+
+        public bool Visited = false;
+
+        public bool IsPath = false;
+
+        public Node Parent = null;
+
+        public Node(int r, int c)
         {
-
-            public int Row;
-            public int Col;
-
-            public bool IsWall = false;
-
-            public bool Visited = false;
-
-            public bool IsPath = false;
-
-            public Node Parent = null;
-
-            public Node(int r, int c)
-            {
-                Row = r;
-                Col = c;
-            }
+            Row = r;
+            Col = c;
         }
     }
-
-
-
+}
